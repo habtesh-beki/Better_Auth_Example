cc0a3ff902ed910d9542bbdc0938555b99a0efcd
habtesh-beki <habteshbaki2014@gmail.com>
2025-04-27 08:12:45 -0400

    add server.ts file

---
+++ server/server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
+import cors from "cors";
+const app = express();
+const port = 3000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.all("/api/auth/{*any}", toNodeHandler(auth));
+
+app.listen(port, () => {
+  console.log(`Better Auth app listening on port ${port}`);
+});
